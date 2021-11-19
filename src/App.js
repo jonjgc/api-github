676d4fc2b4e0ccf57653592372ef50e6b9509611
Jônatas Camelo <46847572+jonjgc@users.noreply.github.com>
2021-11-19 14:25:23 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function App() {
   const [ repositorios, setRepositorios ] = useState([]);
+  const [ busca, setBusca ] = useState("");
+  console.log(busca);
+
+  //const repfiltrados = repositorios.filter((repositorio) => repositorio.startswith(busca));
 
   useEffect(() => {
     fetch('https://api.github.com/users/aws/repos')
@@ -17,11 +21,18 @@ export function App() {
        
        <h2>Listagem de repositórios</h2> 
        
+       <nav class="navbar navbar-light bg-light offset-sm-9">
+          <form class="form-inline offset-sm-2">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          </form>
+        </nav>
+
        <ul>
          { repositorios.map(repositorio => {
            return (
              <ul>
-               <li>  
+               
+               <li key={repositorio}>  
                   <h3>{repositorio.name}</h3>
                   <p><strong>Linguagem:</strong> {repositorio.language}</p>
                   <p><strong>Descrição:</strong> {repositorio.description}</p>
@@ -30,6 +41,7 @@ export function App() {
 
                   <a href={repositorio.html_url} target="_blank" rel="noreferrer">Saiba mais</a>
                </li>
+
              </ul>  
            )
          }) }
@@ -38,4 +50,3 @@ export function App() {
     </div>
   );
 }
-
