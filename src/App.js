9fd1017638767c4ef6fb29c482f8a48624eef580
Jônatas Camelo <46847572+jonjgc@users.noreply.github.com>
2021-11-25 00:02:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,35 +4,54 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function App() {
   const [ repositorios, setRepositorios ] = useState([]);
-  const [ busca, setBusca ] = useState("");
-  console.log(busca);
+  const [ itemsPagina, setItemsPagina ] = useState(2);
+  const [ paginaAtual, setPaginaAtual ] = useState(0)
+  const startRepositorios = paginaAtual * itemsPagina;
+  const endRepositorios = startRepositorios + itemsPagina;
+  const RepositorioAtual = repositorios.slice(startRepositorios, endRepositorios);
 
-  //const repfiltrados = repositorios.filter((repositorio) => repositorio.startswith(busca));
+
+  const paginas = Math.ceil(repositorios.length / itemsPagina);
 
   useEffect(() => {
-    fetch('https://api.github.com/users/aws/repos')
-    .then(response => response.json())
-    .then(data => setRepositorios(data))
+    const fetchData = async () => {
+     const result = await fetch('https://api.github.com/users/aws/repos')
+      .then(response => response.json())
+      .then(data => setRepositorios(data))
+    }
+    fetchData()
   }, []) 
 
   return (
     <div>  
-        
+
+          
+
        
        <h2>Listagem de repositórios</h2> 
-       
-       <nav class="navbar navbar-light bg-light offset-sm-9">
-          <form class="form-inline offset-sm-2">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          </form>
-        </nav>
+
+       <div id="paginacao"> 
+          {Array.from(Array(paginas), (repositorios, index) => {
+            return <button value={index} onClick={(e) => setPaginaAtual(Number(e.target.value))} class="btn btn-primary">{index}</button>
+          })}
+       </div> 
+
+      <div class="col-2 offset-sm-5 mt-4" >
+
+        <form class="form-inline" >
+            <input  class="form-control mr-sm-2" type="search" placeholder="Filtrar" aria-label="Search"/>
+        </form>
+              
+       </div>
 
        <ul>
-         { repositorios.map(repositorio => {
+         { RepositorioAtual.map(repositorio => {
            return (
+
+           
+
              <ul>
-               
-               <li key={repositorio}>  
+               <li>  
                   <h3>{repositorio.name}</h3>
                   <p><strong>Linguagem:</strong> {repositorio.language}</p>
                   <p><strong>Descrição:</strong> {repositorio.description}</p>
@@ -41,11 +60,12 @@ export function App() {
 
                   <a href={repositorio.html_url} target="_blank" rel="noreferrer">Saiba mais</a>
                </li>
-
              </ul>  
            )
          }) }
         </ul>
+
+      
 
     </div>
   );
